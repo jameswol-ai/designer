9956220e441aaa33aff9fe174636adb11e3791cf
James Wol <james.wol@petalmail.com>
2026-09-05 14:18:46 +0200

    Expose modern versioned data contract

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,4 +1,5 @@
 from .models import Project, Space
+from .data_contract import SCHEMA_VERSION, project_document, normalize_layout, validate_document
 from .planner import generate_program, build_project
 from .adjacency import adjacency_matrix, adjacency_score, adjacency_summary
 from .layout import generate_layout
@@ -16,11 +17,10 @@ from .building_generator import BuildingModel, generate_building, building_summa
 from .validation import check_project, score_project
 
 __all__ = [
-    "Project", "Space", "generate_program", "build_project",
-    "adjacency_matrix", "adjacency_score", "adjacency_summary",
-    "generate_layout", "generate_alternatives", "best_alternative",
-    "planning_summary", "classify_zone", "required_adjacencies",
-    "adjacency_proximity_score", "overlap_penalty", "zoning_score",
-    "grid_alignment_score", "constraint_report", "BuildingModel",
-    "generate_building", "building_summary", "check_project", "score_project",
+    "Project", "Space", "SCHEMA_VERSION", "project_document", "normalize_layout", "validate_document",
+    "generate_program", "build_project", "adjacency_matrix", "adjacency_score", "adjacency_summary",
+    "generate_layout", "generate_alternatives", "best_alternative", "planning_summary",
+    "classify_zone", "required_adjacencies", "adjacency_proximity_score", "overlap_penalty",
+    "zoning_score", "grid_alignment_score", "constraint_report", "BuildingModel", "generate_building",
+    "building_summary", "check_project", "score_project",
 ]
