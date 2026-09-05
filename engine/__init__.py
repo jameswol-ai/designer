1280ea38a25151c3aacaf5310742e6dda93e4339
James Wol <james.wol@petalmail.com>
2026-09-05 13:55:43 +0200

    Export building generation engine

--- engine/__init__.py
+++ engine/__init__.py
@@ -2,22 +2,25 @@ from .models import Project, Space
 from .planner import generate_program, build_project
 from .adjacency import adjacency_matrix, adjacency_score, adjacency_summary
 from .layout import generate_layout
+from .space_planning import generate_alternatives, best_alternative, planning_summary
 from .planning_constraints import (
-    adjacency_proximity_score,
     classify_zone,
-    constraint_report,
-    grid_alignment_score,
+    required_adjacencies,
+    adjacency_proximity_score,
     overlap_penalty,
     zoning_score,
+    grid_alignment_score,
+    constraint_report,
 )
-from .space_planning import generate_alternatives, best_alternative, planning_summary
+from .building_generator import BuildingModel, generate_building, building_summary
 from .validation import check_project, score_project
 
 __all__ = [
     "Project", "Space", "generate_program", "build_project",
     "adjacency_matrix", "adjacency_score", "adjacency_summary",
     "generate_layout", "generate_alternatives", "best_alternative",
-    "planning_summary", "check_project", "score_project",
-    "classify_zone", "adjacency_proximity_score", "zoning_score",
-    "grid_alignment_score", "overlap_penalty", "constraint_report",
+    "planning_summary", "classify_zone", "required_adjacencies",
+    "adjacency_proximity_score", "overlap_penalty", "zoning_score",
+    "grid_alignment_score", "constraint_report", "BuildingModel",
+    "generate_building", "building_summary", "check_project", "score_project",
 ]
