d1dbf5bab2d0aa88fc95d0997baf03906c52d976
James Wol <james.wol@petalmail.com>
2026-09-05 13:40:58 +0200

    Export intelligent space planning engine

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,7 +1,13 @@
 from .models import Project, Space
 from .planner import generate_program, build_project
-from .adjacency import adjacency_matrix
+from .adjacency import adjacency_matrix, adjacency_score, adjacency_summary
 from .layout import generate_layout
+from .space_planning import generate_alternatives, best_alternative, planning_summary
 from .validation import check_project, score_project
 
-__all__ = ["Project", "Space", "generate_program", "build_project", "adjacency_matrix", "generate_layout", "check_project", "score_project"]
+__all__ = [
+    "Project", "Space", "generate_program", "build_project",
+    "adjacency_matrix", "adjacency_score", "adjacency_summary",
+    "generate_layout", "generate_alternatives", "best_alternative",
+    "planning_summary", "check_project", "score_project",
+]
