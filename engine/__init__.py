6c33233367ef86bdcad76f4f18bdefda842462ad
James Wol <james.wol@petalmail.com>
2026-09-05 13:50:13 +0200

    Export planning constraint utilities

--- engine/__init__.py
+++ engine/__init__.py
@@ -2,6 +2,14 @@ from .models import Project, Space
 from .planner import generate_program, build_project
 from .adjacency import adjacency_matrix, adjacency_score, adjacency_summary
 from .layout import generate_layout
+from .planning_constraints import (
+    adjacency_proximity_score,
+    classify_zone,
+    constraint_report,
+    grid_alignment_score,
+    overlap_penalty,
+    zoning_score,
+)
 from .space_planning import generate_alternatives, best_alternative, planning_summary
 from .validation import check_project, score_project
 
@@ -10,4 +18,6 @@ __all__ = [
     "adjacency_matrix", "adjacency_score", "adjacency_summary",
     "generate_layout", "generate_alternatives", "best_alternative",
     "planning_summary", "check_project", "score_project",
+    "classify_zone", "adjacency_proximity_score", "zoning_score",
+    "grid_alignment_score", "overlap_penalty", "constraint_report",
 ]
